2766fd0538e43fb1185c2e71549c5c8e3bfa74a6
Ian <ianrdejong@gmail.com>
2021-10-17 01:27:12 -0700

    Display input data as bar chart

--- js/main.js
+++ js/main.js
@@ -13,6 +13,10 @@ let canvas;
 let input_parsed = [];
 
 
+// Style variables
+let bar_padding_top = 25; // The space between the top of the tallest bar and the top of the window (px)
+
+
 function setup() {
   let parentStyle = window.getComputedStyle(document.getElementById(ID_PARENT));
   canvas = createCanvas(parseInt(parentStyle.width), parseInt(parentStyle.height));
@@ -26,8 +30,6 @@ function setup() {
   //INTERFACE_RATE.onchange = update speed;
 
   parseInputData();
-
-  render();
 }
 
 
@@ -46,7 +48,7 @@ function parseInputData(){
   let isPrevCharNumber = false;
   for(let i = 0; i < raw_data.length; i++){
     let char = raw_data[i];
-    if(!isNaN(char)){ // Current char is a number
+    if(!isNaN(char) && char != ' '){ // Current char is a number
       char = parseInt(char);
       if(isPrevCharNumber){
         number *= 10;
@@ -90,6 +92,18 @@ function draw(){
 function render()
 {
   background(BG_COL);
+  fill(0);
+  stroke(BG_COL);
+  strokeWeight(2);
+  let number_of_bars = input_parsed.length
+  if(number_of_bars > 0){
+    let bar_width = width / number_of_bars;
+    let max_number = Math.max(...input_parsed);
+    for(let i = 0; i < number_of_bars; i++){
+      let bar_height = map(input_parsed[i], 0, max_number, 0, height - bar_padding_top);
+      rect(bar_width * i, height - bar_height, bar_width, bar_height);
+    }
+  }
 }
 
 
